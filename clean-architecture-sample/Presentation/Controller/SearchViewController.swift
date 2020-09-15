0b0b0ed2c001879923602ad94600835b88e31565
temma.io <eifan.devs@gmail.com>
2020-09-16 00:16:05 +0900

    add snapkit

---
+++ clean-architecture-sample/Presentation/Controller/SearchViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SearchViewController.swift
+//  clean-architecture-sample
+//
+//  Created by iori tenma on 2020/09/15.
+//  Copyright © 2020 iori tenma. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
